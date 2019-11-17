03c676fd586b1d9dad83d9f203b85cab4916f4c4
woyong <woyongegbara@yahoo.com>
2019-11-17 20:41:39 +0100

    Code update

--- src/api/esp8266Api.js
+++ src/api/esp8266Api.js
@@ -4,12 +4,12 @@ class esp8266Api {
   static updateEsp8266(brightness, ip) {
     return axios({
       method: 'post',
-      url: `http://${ip}/body`,
+      url: `http://${ip.ip}/body`,
       timeout: 5 * 2000,
       data: JSON.stringify(brightness),
     })
       .then(({ data }) => data)
-      .catch(error => error);
+      .catch(error => console.log(error));
   }
 }
 
